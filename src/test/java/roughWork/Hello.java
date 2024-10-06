b6a187009f359440b213a70b540b3b7fdb222cc2
Arman3199 <rmanahmed946@gmail.com>
2024-10-06 12:42:09 -0400

    Update Hello class w implementation

--- src/test/java/roughWork/Hello.java
+++ src/test/java/roughWork/Hello.java
@@ -3,7 +3,10 @@ package roughWork;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Our mdoule is finishing");
+		System.out.println("Our modoule is finishing");
+		//this classs developed by us
+		//im  optimistic about our success
+		
 	}
 
 }
